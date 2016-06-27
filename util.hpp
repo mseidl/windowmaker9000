65bf0d2be1175356d7cb394f64395a973cc34af7
Martin Seidl <mseidl@suse.de>
2016-06-27 15:37:56 +0200

    temp garbage

--- util.hpp
+++ util.hpp
@@ -5,6 +5,8 @@ extern "C" {
 #include  X11/Xlib.h
 }
 #include <string>
+#include <sstream>
+#include <ostream>
 
 template<typename T>
 struct Size
@@ -17,6 +19,23 @@ struct Size
 
   std::string ToString() const;
 };
+
+template<typename T>
+std::ostream& operator<< (std::ostream& out, const Size<T>& size)
+{
+  return out << size.ToString();
+}
+
+template<typename T>
+Size<T> operator- (const 
+
+template<typename T>
+std::string Size<T>::ToString() const
+{
+  std::ostringstream out;
+  out << width << 'x' << height;
+  return out.str();
+}
 
 template <typename T>
 struct Position
@@ -31,6 +50,20 @@ struct Position
 };
 
 template <typename T>
+std::ostream& operator<< (std::ostream& out, Position<T>& pos)
+{
+  return out << pos.ToString();
+}
+
+template <typename T>
+std::string Position<T>::ToString() const
+{
+  std::ostringstream out;
+  out << '(' << x << ", " << y << ')';
+  return out.str();
+}
+
+template <typename T>
 struct Vector2D
 {
   T x;
@@ -41,5 +74,19 @@ struct Vector2D
    
   std::string ToString() const;
 };
+
+template<typename T>
+std::ostream& operator<< (std::ostream& out, Vector2D<T>& vec)
+{
+  return out << vec.ToString();
+}
+
+template <typename T>
+std::string Vector2D<T>::ToString() const
+{
+  std::ostringstream out;
+  out << '(' << x << ", " << y << ')';
+  return out.str();
+}
 
 #endif // UTIL_HPP
